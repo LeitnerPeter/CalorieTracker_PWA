2820581737ec75ff370144f418582c2d0e3be61b
LeitnerPeter <43432842+LeitnerPeter@users.noreply.github.com>
2026-04-18 06:37:03 +0200

    Change supdabase to Services

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-const supabaseUrl = "https://xyakkemkejxnpdzopmbb.supabase.co";
-const supabaseKey = "sb_publishable_ZzFGVRc3XUU7QnFR364VQg_UwAmO1Zm";
-
-export const supabaseClient = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
